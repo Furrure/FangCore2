81c18df7318032df01bad9f295ba256e868736e1
Jacob Scrapchansky <71794171+Furrure@users.noreply.github.com>
2022-06-28 09:20:09 -0400

    Fixed Docs generator for github pages

--- generate_and_view_documentation.bat
+++ generate_and_view_documentation.bat
@@ -1,1 +1,5 @@
-py -3 -m pdoc --html --force --output-dir .\docs .\FangCore2 && .\docs\FangCore2\index.html
+py -3 -m pdoc --html --force --output-dir .\docs .\FangCore2
+xcopy /s/e .\docs\FangCore2\* .\docs
+rmdir /s .\docs\FangCore2
+
+.\docs\index.html
