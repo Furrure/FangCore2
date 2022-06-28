8c0b37f9ee467e82c0d1ce4ea1c58027d814ac89
Jacob Scrapchansky <71794171+Furrure@users.noreply.github.com>
2022-06-28 10:08:08 -0400

    added split and index_all

--- FangCore2/Strings.py
+++ FangCore2/Strings.py
@@ -4,9 +4,10 @@ All String related FangCore2 tools
 from typing import *
 
 
-def extract_string_arguments(*args: Union[str, list[Union[str, any]]]) -> list[str]:
+def extract_string_arguments(*args: Union[str, list[Union[str, any]]]) -> tuple[str]:
     """
-    When given strings, a list of strings, or a combination of both, will extract all of these strings into one list
+    When given strings, a list of strings, or a combination of both, will extract all of these strings into one list.
+    This function is used by most `FangCore2.Strings` functions and methods.
     """
     string_arguments = []
     for arg in args:
@@ -16,16 +17,73 @@ def extract_string_arguments(*args: Union[str, list[Union[str, any]]]) -> list[s
         else:
             string_arguments.append(str(arg))
 
-    return string_arguments
+    return tuple(string_arguments)
 
 
-def split(string: str, *args: Union[str, list[Union[str, any]]]) -> list[str]:
+def split(string: str, *args: Union[str, list[Union[str, any]]]) -> tuple[str]:
     """
-    Separate a string by specified strings are characters
+    Separate a string by specified strings or characters.
+
+    Order of arguments is the order of character detection and splitting.
+    ex: `split("Hello", "ll", "ell") => ["He", "o"]`
     """
 
-    string = str(string)
-    split_strings = extract_string_arguments(*args)
+    string = str(string)  # Input string
+
+    split_strings = extract_string_arguments(*args)  # Strings that the input string will be split by
+
+    output = []  # The result of the split operation
+
+    working_word = ""  # The next word that is being appended to until a split sequence is reached
+
+    char_index = 0  # The current character that is being read
+    while not char_index >= len(string):
+        for splitter in split_strings:
+            if len(splitter) > len(string[char_index:]):  # Check if the length even matches
+                continue
+            elif string[char_index:char_index+len(splitter)] == splitter:  # Check for a match in the string and break if so
+                if working_word.strip() != "":
+                    output.append(working_word)
+                char_index = char_index+len(splitter)
+                working_word = ""
+                break
+        if not char_index >= len(string):
+            working_word += string[char_index]
+        char_index += 1
+
+    if working_word.strip() != "":
+        output.append(working_word)
+
+    return tuple(output)
+
+
+def index_all(string: str, *args: Union[str, list[Union[str, any]]]) -> tuple[int]:
+    """
+    Index all the occurrences of the given strings
+
+    If there are multiple matches at one index, the index is only included once.
+    """
+
+    string = str(string)  # Input string
+
+    index_strings = extract_string_arguments(*args)  # Strings that the input string will be indexed
+
+    output = []  # The result of the index operation
+
+    char_index = 0  # The current character that is being read
+    while not char_index >= len(string):
+        for indexer in index_strings:
+            if len(indexer) > len(string[char_index:]):  # Check if the length even matches
+                continue
+            elif string[char_index:char_index + len(indexer)] == indexer:  # Check for a match in the string and break if so
+                if char_index not in output:
+                    output.append(char_index)
+                break
+        char_index += 1
+
+    return tuple(output)
+
+
 
 
 
--- tests.py
+++ tests.py
@@ -10,8 +10,28 @@ def strings_extract_string_arguments():
         raise RuntimeError("Incorrect result")
 
 
+def strings_split():
+    try:
+        result = FangCore2.Strings.split("hello there!", " ", ["e!", "hel"])
+    except Exception as exception:
+        raise exception
+    if result != ["lo", "ther"]:
+        raise RuntimeError("Incorrect result")
+
+
+def strings_index_all():
+    try:
+        result = FangCore2.Strings.index_all("hello there!", " ", ["e!", "hel"])
+    except Exception as exception:
+        raise exception
+    if result != [0, 5, 10]:
+        raise RuntimeError("Incorrect result")
+
+
 test_functions = {
-    "Strings.extract_string_arguments" : strings_extract_string_arguments
+    "Strings.extract_string_arguments": strings_extract_string_arguments,
+    "Strings.split": strings_split,
+    "Strings.index_all": strings_index_all
 
 }
 
